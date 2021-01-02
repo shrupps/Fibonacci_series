61b53a3301426c4760df6c344311751ad1d3642c
shrupps <63329778+shrupps@users.noreply.github.com>
2021-01-02 16:32:59 +0530

    Create hello.py
    this file print fibonacci series without asking user

---
+++ hello.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    b = 1
+    a = 0
+    while a < 145:
+          print(a, end=' , ')
+          c = a + b
+          a = b
+          b = c
+
+          a+=0
+
+print(fibonacci(0))
